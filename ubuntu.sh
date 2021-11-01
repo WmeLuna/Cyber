95d92f299b6e3bc0444b73860c8d31c997db0bee
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2021-11-01 13:53:03 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,3 @@
-echo "nospoof on" | sudo tee -a /etc/host.conf
 sudo sed -i -e 's/APT::Periodic::Update-Package-Lists.*\+/APT::Periodic::Update-Package-Lists "1";/' /etc/apt/apt.conf.d/10periodic
 sudo sed -i -e 's/APT::Periodic::Download-Upgradeable-Packages.*\+/APT::Periodic::Download-Upgradeable-Packages "0";/' /etc/apt/apt.conf.d/10periodic
 sudo apt update
@@ -11,8 +10,12 @@ sudo apt-fast install -y gnome-session-fallback ssh ufw unattended-upgrades rkhu
 sudo apt-fast upgrade -y
 sudo ufw enable
 sudo ufw logging on
+
+## lock root user
 sudo passwd -l root
 
+## disable guest
+echo "allow-guest=false" >> /etc/lightdm/lightdm.conf
 
 # password security
 sudo sed 's/pam_cracklib.so/pam_cracklib.so ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1 /g' /etc/pam.d/common-password
@@ -28,11 +31,23 @@ rkhunter -c --sk
 lynis -c --quick
 chkrootkit -q
 
+sysctl -n net.ipv4.tcp_syncookies
+echo 0 | sudo tee /proc/sys/net/ipv4/ip_forward
+echo "nospoof on" | sudo tee -a /etc/host.conf
+
+
+clear
+
+echo "Start printing text that may be useful to get more points"
 ## list games ##
+echo "List games"
 dpkg -l | grep -i game
 
-## lock root user
-sudo passwd -l root
+echo " "
+echo "List files in all home dirs"
+ls /home/*/*
 
-## disable guest
-echo "allow-guest=false" >> /etc/lightdm/lightdm.conf
+echo " "
+echo "Listing all human users"
+cut -d: -f1,3 /etc/passwd | egrep ':[0-9]{4}$' | cut -d: -f1
+
