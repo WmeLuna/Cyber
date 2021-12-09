3c7304533b5c358cee8bfd5f5e6e50d2e9f2eda1
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2021-12-09 18:10:00 -0500

    Update apt-smart.sh

--- apt-smart.sh
+++ apt-smart.sh
@@ -4,4 +4,4 @@ sudo apt install python3-pip python-setuptools python3-wheel -y  # install pytho
 pip install --user apt-smart  # --user flag means install to per user site-packages directory(see below)
 echo "export PATH=\$(python -c 'import site; print(site.USER_BASE + \"/bin\")'):\$PATH" >> ~/.bashrc
 source ~/.bashrc  # set per user site-packages directory to PATH
-apt-smart -a
+apt-smart -aq
