115e979eccebab1f1988a4e8d1213b0f82259ddf
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2021-12-09 17:15:31 -0500

    Update updates.sh

--- updates.sh
+++ updates.sh
@@ -1,6 +1,6 @@
 cd /etc/apt/apt.conf.d/
-wget -O https://github.com/WmeLuna/Cyber/raw/main/10periodic 10periodic
-wget -O https://github.com/WmeLuna/Cyber/raw/main/20auto-upgrades 20auto-upgrades
+curl -sL https://github.com/WmeLuna/Cyber/raw/main/10periodic > 10periodic
+curl -sL https://github.com/WmeLuna/Cyber/raw/main/20auto-upgrades > 20auto-upgrades
 # lsb_release -cs
 echo -e "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs) main universe restricted multiverse" > /etc/apt/sources.list
 echo -e "deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -cs) main universe restricted multiverse " >> /etc/apt/sources.list
