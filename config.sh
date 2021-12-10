195184acdf5d1d728d85f586aff049647e08dfa2
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2021-12-10 18:53:08 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,5 @@
-curl -sL https://github.com/Spyromaniac31/cyberpatriots-ubuntu/raw/main/script/common-password > /etc/pam.d/common-password
-curl -sL https://github.com/Spyromaniac31/cyberpatriots-ubuntu/raw/main/script/limits.conf > /etc/security/limits.conf
-curl -sL https://github.com/Spyromaniac31/cyberpatriots-ubuntu/raw/main/script/login.defs > /etc/login.defs
-curl -sL https://github.com/Spyromaniac31/cyberpatriots-ubuntu/raw/main/script/sysctl.conf > /etc/security/limits.conf
+curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/common-auth > /etc/pam.d/common-auth
+curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/common-password > /etc/pam.d/common-password
+curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/limits.conf > /etc/security/limits.conf
+curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/login.defs > /etc/login.defs
+curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/sysctl.conf > /etc/sysctl.conf
