a1bdadce34d73f1021d8a26c68bce958699e5287
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2021-12-10 18:56:32 -0500

    Update lynis.sh

--- lynis.sh
+++ lynis.sh
@@ -16,29 +16,3 @@ echo "install dccp /bin/true" > /etc/modprobe.d/dccp.conf
 echo "install rds /bin/true" > /etc/modprobe.d/rds.conf
 echo "install tipc /bin/true" > /etc/modprobe.d/tipc.conf
 
-apt-get -y purge gnome-games > /dev/null
-
-rm -rf /usr/games > /dev/null
-rm -rf /usr/local/games > /dev/null
-
-chmod o-rx /usr/bin/x86_64-linux-gnu-as
-
-chown root:shadow /etc/shadow
-chmod 640 /etc/shadow
-
-chown root:root /etc/passwd
-chmod 644 /etc/passwd
-
-chown root:root /etc/group
-chmod 644 /etc/group
-chown root:root /etc/pam.d
-chmod 644 /etc/pam.d
-chown root:shadow /etc/gshadow
-chmod 640 /etc/gshadow
-chmod 600 /etc/crontab
-chmod 700 /etc/cron.d
-chmod 700 /etc/cron.daily
-chmod 700 /etc/cron.hourly
-chmod 700 /etc/cron.monthly
-chmod 700 /etc/cron.weekly
-chmod 600 /etc/cups/cupsd.conf
