954f0c7f9bac82dba2443956effae7c4477ab2b6
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2021-12-10 18:57:28 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,3 +3,30 @@ curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/common-password > /etc/p
 curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/limits.conf > /etc/security/limits.conf
 curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/login.defs > /etc/login.defs
 curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/sysctl.conf > /etc/sysctl.conf
+
+apt-get -y purge gnome-games > /dev/null
+
+rm -rf /usr/games > /dev/null
+rm -rf /usr/local/games > /dev/null
+
+chmod o-rx /usr/bin/x86_64-linux-gnu-as
+
+chown root:shadow /etc/shadow
+chmod 640 /etc/shadow
+
+chown root:root /etc/passwd
+chmod 644 /etc/passwd
+
+chown root:root /etc/group
+chmod 644 /etc/group
+chown root:root /etc/pam.d
+chmod 644 /etc/pam.d
+chown root:shadow /etc/gshadow
+chmod 640 /etc/gshadow
+chmod 600 /etc/crontab
+chmod 700 /etc/cron.d
+chmod 700 /etc/cron.daily
+chmod 700 /etc/cron.hourly
+chmod 700 /etc/cron.monthly
+chmod 700 /etc/cron.weekly
+chmod 600 /etc/cups/cupsd.conf
