f5b465fbf213000afc63cd6d7af4468193f3e2dc
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2021-12-10 12:16:11 -0500

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,4 @@
+curl -sL https://github.com/Spyromaniac31/cyberpatriots-ubuntu/raw/main/script/common-password > /etc/pam.d/common-password
+curl -sL https://github.com/Spyromaniac31/cyberpatriots-ubuntu/raw/main/script/limits.conf > /etc/security/limits.conf
+curl -sL https://github.com/Spyromaniac31/cyberpatriots-ubuntu/raw/main/script/login.defs > /etc/login.defs
+curl -sL https://github.com/Spyromaniac31/cyberpatriots-ubuntu/raw/main/script/sysctl.conf > /etc/security/limits.conf
