4df0eb6e1ac1b240d90799e8c3fa3e933128957e
WmeLuna <warriorallen11@gmail.com>
2021-12-20 18:28:14 -0500

    add audit.rules to comply with ACCT-9630

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/common-password > /etc/p
 curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/limits.conf > /etc/security/limits.conf
 curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/login.defs > /etc/login.defs
 curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/sysctl.conf > /etc/sysctl.conf
-
+curl -sL https://github.com/WmeLuna/Cyber/raw/main/conf/audit.rules > /etc/audit/rules.d/audit.rules
 
 apt-get install auditd -y
 sed 's/log_file = .*/log_file = /var/log/audit/audit.log/g' /etc/audit/auditd.conf
