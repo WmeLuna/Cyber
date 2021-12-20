49130925b031a780d5f7ed436aee6f86e3f04bad
WmeLuna <warriorallen11@gmail.com>
2021-12-20 18:09:56 -0500

    fix  theme

--- nordic-theme.sh
+++ nordic-theme.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd ~ && mkdir .themes && cd ~/.themes 
+cd ~ && mkdir .themes 
+cd ~/.themes 
 curl -LOJ https://github.com/EliverLara/Nordic/releases/download/v2.1.0/Nordic.tar.xz
 tar -xf Nordic.tar.xz
 gsettings set org.gnome.desktop.interface gtk-theme Nordic
