001ba494ce6e5f8f20f601cdec1c5fc4f493cb53
WmeLuna <warriorallen11@gmail.com>
2021-12-20 23:27:38 -0500

    commit

--- nordic-theme.sh
+++ nordic-theme.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 sudo add-apt-repository ppa:papirus/papirus -y
-sudo apt-get install tar gnome-shell-extensions gnome-tweaks papirus-icon-theme
+sudo apt-get install -y tar gnome-shell gnome-shell-extensions gnome-tweaks papirus-icon-theme
 
 cd ~ && mkdir .themes 
 cd ~/.themes 
 
-status=$(gnome-extensions info user-theme@gnome-shell-extensions.gcampax.github.com | grep -o "State:.*" | cut -f2- -d:)
-if [ "$status" = " DISABLED" ]; then 
+if [ "$(lsb_release -is)" = "Debian" ]; then 
+    sudo apt-get install -y gnome-shell-extension-dashtodock
+    gnome-shell-extension-tool -e user-theme@gnome-shell-extensions.gcampax.github.com
+    gnome-shell-extension-tool -e dash-to-dock@micxgx.gmail.com
+    gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed true
+    gsettings set org.gnome.shell.extensions.dash-to-dock extend-height true
+else
     gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
-
 fi
+
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode DYNAMIC
 curl -LOJ https://github.com/EliverLara/Nordic/releases/download/v2.1.0/Nordic.tar.xz
 tar -xf Nordic.tar.xz
 gsettings set org.gnome.desktop.interface gtk-theme Nordic
