0bc0c85626cb8a17ecd04b483c0718d6a7fbc814
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-10-18 08:44:50 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#echo "This is a script made by Allen Martinez (luna@wmeluna.com) for CyberPatriot team 14-4293"
-#echo "Any Use of this script that is not this team is NOT allowed!"
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' > /dev/null 2>&1 #darkmde bc i like my eyes
 echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$USER > /dev/null 2>&1 # disable sudo prompt during the comp
 
