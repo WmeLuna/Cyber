8eba9f229dd5fda0e15a9d08a659e283faaf250f
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-10-21 17:01:59 -0400

    Update lynis.sh

--- lynis.sh
+++ lynis.sh
@@ -1,8 +1,9 @@
 wget -q -O - https://packages.cisofy.com/keys/cisofy-software-public.key | sudo apt-key add - &> /dev/null
 echo "deb https://packages.cisofy.com/community/lynis/deb/ stable main" | sudo tee /etc/apt/sources.list.d/cisofy-lynis.list > /dev/null
 echo 'Acquire::Languages "none";' | sudo tee /etc/apt/apt.conf.d/99disable-translations
-apt-fast -y install apt-transport-https sysstat acct auditd debsums apt-show-versions lynis
 apt-fast update
+apt-fast -y install apt-transport-https sysstat acct auditd debsums apt-show-versions lynis
+
 
 sudo echo "Package: lynis
 Pin: origin packages.cisofy.com
